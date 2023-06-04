19a7b871f5ea9b5f965873aefc20f7be801e7c3a
chazari <chalimovrz@gmail.com>
2023-06-04 20:12:04 +0500

    goroutines

--- worker/internal/app/worker/worker.go
+++ worker/internal/app/worker/worker.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"log"
 	"strings"
+	"sync"
 	"time"
 
 	"worker/internal/app/config"
@@ -40,12 +41,28 @@ func StartWorker() error {
 		b: bot,
 	}
 
-	var ch chan error
+	c.mainWorker()
 
-	c.worker(ch)
+	var wg sync.WaitGroup
 
-	for err = range ch {
-		log.Print(err)
+	go func() {
+		for true {
+			if workerOne.work {
+				wg.Add(1)
+				c.worker(&wg, &workerOne)
+			}
+
+			wg.Wait()
+		}
+	}()
+
+	for true {
+		if workerTwo.work {
+			wg.Add(1)
+			c.worker(&wg, &workerTwo)
+		}
+
+		wg.Wait()
 	}
 
 	log.Print("service stopped")
@@ -53,21 +70,88 @@ func StartWorker() error {
 	return nil
 }
 
-func (c *Controller) worker(ch chan<- error) {
+var (
+	workerOne = workerStruct{work: false, nameNewWorker: "worker two"}
+	workerTwo = workerStruct{work: false, nameNewWorker: "worker three"}
+)
+
+type workerStruct struct {
+	work          bool
+	nameNewWorker string
+}
+
+func (c *Controller) mainWorker() {
 	go func() {
 		log.Print("starting goroutine")
 
 		defer func() {
-			c.worker(ch)
+			c.mainWorker()
 		}()
 
 		for {
+			workerOne.work = false
+
 			get, err := c.r.GetMessage("maya")
 			if err != nil {
-				go func() {
-					ch <- err
-				}()
+				log.Printf("worker get message err: %s", err)
 				continue
+			}
+
+			if get.Body == nil {
+				continue
+			}
+
+			workerOne.work = true
+
+			if get.Type != "" {
+				log.Printf("[%s] Уведомление для %s от %s: %s\n",
+					strings.ToUpper(get.Type),
+					get.ReplyTo,
+					get.Timestamp.Format(time.RFC822),
+					get.Body)
+			}
+
+			switch strings.ToLower(get.Type) {
+			case "telegram":
+				if err := c.b.SendMessage(get); err != nil {
+					log.Printf("worker send message err: %s", err)
+					if err := c.r.ReturnMessage(get); err != nil {
+						log.Printf("worker return message err: %s", err)
+						continue
+					}
+				}
+			}
+		}
+	}()
+}
+
+func (c *Controller) worker(wg *sync.WaitGroup, w *workerStruct) {
+	go func() {
+		log.Printf("starting %s", w.nameNewWorker)
+
+		for {
+			if w.nameNewWorker == workerOne.nameNewWorker {
+				w.work = false
+			}
+
+			if !w.work {
+				log.Printf("%s is closed", w.nameNewWorker)
+				wg.Done()
+				break
+			}
+
+			get, err := c.r.GetMessage("maya")
+			if err != nil {
+				log.Printf("worker get message err: %s", err)
+				continue
+			}
+
+			if get.Body == nil {
+				continue
+			}
+
+			if w.nameNewWorker == workerOne.nameNewWorker {
+				w.work = true
 			}
 
 			if get.Type != "" {
@@ -81,10 +165,9 @@ func (c *Controller) worker(ch chan<- error) {
 			switch strings.ToLower(get.Type) {
 			case "telegram":
 				if err := c.b.SendMessage(get); err != nil {
+					log.Printf("worker send message err: %s", err)
 					if err := c.r.ReturnMessage(get); err != nil {
-						go func() {
-							ch <- err
-						}()
+						log.Printf("worker return message err: %s", err)
 						continue
 					}
 				}
